d0896c0ef4cb5058f451d6171e58bb721ce92e7f
Erik Blankinship <erikb@mediamods.com>
2023-10-22 23:34:59 -0400

    whitespace

--- app/user/layout.js
+++ app/user/layout.js
@@ -29,7 +29,7 @@ export default function Layout({
         <div 
           className="flex-grow w-100 h-100 flex items-stretch justify-center items-center">
           { children }
-      </div>
+        </div>
     </div>
   );
 };

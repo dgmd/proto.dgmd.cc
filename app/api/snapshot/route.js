5bd46552421681bd134a4951ddbaef1c826ad79c
Erik Blankinship <erikb@mediamods.com>
2024-02-22 11:26:10 -0500

    pass along error

--- app/api/snapshot/route.js
+++ app/api/snapshot/route.js
@@ -2,6 +2,7 @@ import {
   createClient
 } from '@/utils/supabase/server.js';
 import {
+  QUERY_RESPONSE_KEY_ERROR,
   QUERY_RESPONSE_KEY_SUCCESS
 } from 'constants.dgmd.cc';
 import {
@@ -34,30 +35,11 @@ export async function GET( request ) {
     return NextResponse.json( snapshot );
   }
   catch (e) {
-    console.error( e );
+    return NextResponse.json( {
+      [QUERY_RESPONSE_KEY_ERROR]: e.message,
+      [QUERY_RESPONSE_KEY_SUCCESS]: false
+    } );
   }
-  return NextResponse.json( {
-    [QUERY_RESPONSE_KEY_SUCCESS]: false
-  } );
 
-  // const protoSupa = await supabase
-  // .from( 'project_archive' )
-  // .select( 'snapshot, created_at' )
-  // .eq( 'url_id', values[PROTOTYPE_ID] )
-  // .limit( 1 );
-
-  // const proto = protoSupa.data[0];
-  // proto.snapshot[PROTO_RESPONSE_KEY_SNAPSHOT_TIMESTAMP] = proto['created_at'];
-
-  // const resJson = NextResponse.json(
-  //   proto.snapshot
-  // );
-
-  // const headersList = getApiCoriHeaders( request );
-  // for (const header of headersList) {
-  //   resJson.headers.set( header[0], header[1] );
-  // }
-
-  return NextResponse.json( rjson );
 };
 

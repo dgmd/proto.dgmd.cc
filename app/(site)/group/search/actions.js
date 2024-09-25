98e4613268a723b4da312d5b9896428432574586
Erik Blankinship <erikb@mediamods.com>
2024-09-25 13:55:26 -0400

    student sign in

--- app/(site)/group/search/actions.js
+++ app/(site)/group/search/actions.js
@@ -1,8 +1,14 @@
 "use server"
 
 import {
+  createClient
+} from '@/utils/supabase/server.js';
+import {
   isNil
 } from 'lodash-es';
+import {
+  cookies
+} from 'next/headers';
 import {
   redirect
 } from 'next/navigation';
@@ -23,25 +29,25 @@ export async function studentCodeAction( formData ) {
     if (lastHyphenIndex === -1) {
       throw new Error('Invalid student code');
     }
-    const notionGroupId = studentCodeTrim.substring(0, lastHyphenIndex);
+    const notionGroupTitle = studentCodeTrim.substring(0, lastHyphenIndex);
     const notionUserId = studentCodeTrim.substring(lastHyphenIndex + 1);
 
-    // const cookieStore = cookies();
-    // const supabase = await createClient( cookieStore );
-    // const rosterResult = await supabase
-    //   .from( 'rosters' )
-    //   .select( 'notion_id' )
-    //   .eq( 'active', true )
-    //   .eq( 'snapshot_name', snapshotName );
-    // if (!isNil(rosterResult.error)) {
-    //   throw new Error('Error getting roster');
-    // }
-    // const rosters = rosterResult.data;
-    // if (rosters.length === 0) {
-    //   throw new Error('Invalid snapshot name');
-    // }
-    // const roster = rosters[0];
-    // const rosterNotionId = roster.notion_id;
+    const cookieStore = cookies();
+    const supabase = await createClient( cookieStore );
+    const rosterResult = await supabase
+      .from( 'rosters' )
+      .select( 'notion_id' )
+      .eq( 'active', true )
+      .eq( 'snapshot_name', notionGroupTitle );
+    if (!isNil(rosterResult.error)) {
+      throw new Error('Error getting roster');
+    }
+    const rosters = rosterResult.data;
+    if (rosters.length === 0) {
+      throw new Error('Invalid snapshot name');
+    }
+    const roster = rosters[0];
+    const notionGroupId = roster.notion_id;
     redirectUrl = `${ notionGroupId }/${ notionUserId }`;
   }
   catch (error) {

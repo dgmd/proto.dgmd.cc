f370782b3f46d6cb465c214e0f56270914a316e1
Erik Blankinship <erikb@mediamods.com>
2025-02-17 13:17:47 -0500

    update student id to portfolio

--- app/(site)/group/search/actions.js
+++ app/(site)/group/search/actions.js
@@ -1,14 +1,8 @@
 "use server"
 
 import {
-  createClient
-} from '@/utils/supabase/server.js';
-import {
   isNil
 } from 'lodash-es';
-import {
-  cookies
-} from 'next/headers';
 import {
   redirect
 } from 'next/navigation';
@@ -29,26 +23,10 @@ export async function studentCodeAction( formData ) {
     if (lastHyphenIndex === -1) {
       throw new Error('Invalid student code');
     }
-    const notionGroupTitle = studentCodeTrim.substring(0, lastHyphenIndex);
-    const notionUserId = studentCodeTrim.substring(lastHyphenIndex + 1);
+    const notionGroupId = studentCodeTrim.substring(0, lastHyphenIndex).trim();
+    const notionUserId = studentCodeTrim.substring(lastHyphenIndex + 1).trim();
 
-    const cookieStore = await cookies();
-    const supabase = await createClient( cookieStore );
-    const rosterResult = await supabase
-      .from( 'rosters' )
-      .select( 'notion_id' )
-      .eq( 'active', true )
-      .eq( 'snapshot_name', notionGroupTitle );
-    if (!isNil(rosterResult.error)) {
-      throw new Error('Error getting roster');
-    }
-    const rosters = rosterResult.data;
-    if (rosters.length === 0) {
-      throw new Error('Invalid snapshot name');
-    }
-    const roster = rosters[0];
-    const notionGroupId = roster.notion_id;
-    redirectUrl = `${ notionGroupId }/${ notionUserId }`;
+    redirectUrl = `/group/${ notionGroupId }/${ notionUserId }`;
   }
   catch (error) {
     console.log( 'error', error );

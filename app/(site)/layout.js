76d2a7cde57695d620e1e6b7f59edb36ef87240d
Erik Blankinship <erikb@mediamods.com>
2025-12-04 10:24:06 -0500

    tw4 css update

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -12,7 +12,7 @@ export default async function Layout({children}) {
     >
       <Header/>
       <div 
-        className="flex-grow w-100 h-100 flex items-stretch justify-center items-center">
+        className="grow w-full h-full flex items-center justify-center">
         { children }
       </div>
     </div>
